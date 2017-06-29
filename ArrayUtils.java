cf8364058a69a74d322477537ee5f86e877a0750
Sreehari B S <sreeharibs95@gmail.com>
2017-06-29 07:20:24 +0530

    Create ArrayUtils.java

---
+++ ArrayUtils.java
@@ -0,0 +1,30 @@
+package com.ospyn.bubble
+
+import 
+java.util.*;
+
+public class ArrayUtils{
+
+public int[] readArray(int[] array, int n){
+
+for(int i=0;i<n;i++){
+Scanner sc = new Scanner(System.in);
+array[i]=sc.nextInt();
+}
+
+
+
+}
+
+
+public void printArray(int[] array, int n){
+
+for(int i=0;i<n;i++){
+System.out.println(array;
+array[i]=sc.nextInt();
+
+}
+
+}
+}
+
